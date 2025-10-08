764918a07969d425bde74799564a0a2154c1717c
Berk Canpolat <canpolatberk19@gmail.com>
2025-10-08 21:00:37 +0300

    responsive layout added and project finish

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,11 +5,13 @@ import MyButton from "./Button";
 import Nav from "./Nav";
 import { useEffect, useState } from "react";
 import { motion } from "framer-motion";
+import { FaBars } from 'react-icons/fa';
 
 const menuItems = ["Home","Careers","Blog","About Us"]
 
 const Header = () => {
     const [scrolled, setScrolled] = useState(false);
+    const [menuOpen, setMenuOpen] = useState(false);
 
     useEffect(() => {
 
@@ -40,15 +42,16 @@ const Header = () => {
             <img src={logo} alt="logo" />
         </a>
         <nav className="header__nav">
-        <ul className="header__menu">
+        <ul className={`header__menu ${menuOpen ? "active" : ""}`}>
             {
                 menuItems.map((item) => (
                     <Nav key={item} item={item}/>
                 ))
             }
         </ul>
+            <FaBars className="menu" size={20} onClick={() => setMenuOpen((prev) => !prev)}/>
         </nav>
-        <div className="header__auth">
+        <div className={`header__auth ${menuOpen ? "active" : ""}`}>
             <MyButton type="login">Login</MyButton>
             <MyButton type="signup">singup</MyButton>
         </div>
